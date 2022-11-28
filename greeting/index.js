6ecea34b8ebb6decce7855c35697178628154b4e
claire lin <clairelin@kklab.com>
2022-11-28 17:06:12 +0800

    Update index.js

--- greeting/index.js
+++ greeting/index.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
+const args = process.argv.slice(2);
+const currentPath = process.argv[0];
+const configFileName = args[0] ? args[0] : 'greeting.config.js';
+const config = require(`${currentPath}/../../../../../${configFileName}`);
+const { name, drink } = config;
 
-const content = 'Hello, John Doe!';
+const content = `Hello, ${name}! Would you like to have some ${drink}?`;
 fs.writeFile('greeting.txt', content, (error) => {
   if (error) throw error;
   console.log(`Successfully generate file.`);
